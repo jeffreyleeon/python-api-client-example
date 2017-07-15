3675aac4bd1be8831f55de31f9d7294886fffd5f
Lee Chun On <jeffreyleeon@gmail.com>
2017-07-14 19:10:57 -0700

    Add two API call, abstracted in catalog_client.py

--- catalog_client.py
+++ catalog_client.py
@@ -1,3 +1,13 @@
 import requests
 
 base_url = "http://catalog.data.gov/api/3/"
+
+def get_version():
+    url = base_url
+    return requests.get(url)
+
+def search_package(text):
+    # Maybe check whether text is a string?
+    url = '{0}action/package_search?q={1}'.format(base_url, str(text))
+    print('url is {}'.format(url))
+    return requests.get(url)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import catalog_client
 
 print('Beginning of example')
+
+# Request for catalog data version
+version_response = catalog_client.get_version()
+print('This is the version number {}'.format(version_response.json()['version']))
+
+# Search for a package
+search_spending_response = catalog_client.search_package('spending')
+search_test_response = catalog_client.search_package('test')
+print('This is the response of searching spending: {}'.format(search_spending_response.json()))
+print('This is the response of searching test: {}'.format(search_test_response.json()))
